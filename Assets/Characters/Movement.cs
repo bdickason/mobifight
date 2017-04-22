719fc271fee33b7bd0f5b311428692ee30654a19
bdickason <dickason@gmail.com>
2017-04-21 20:46:13 -0700

    Added simple character that can move around the world

---
+++ Assets/Characters/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	[HideInInspector] public bool jump = false;
+	public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+	private bool facingRight = true;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis("Horizontal");
+
+		if (h * rb2d.velocity.x < maxSpeed) {
+            rb2d.AddForce(Vector2.right * h * moveForce);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip ();
+        else if (h < 0 && facingRight)
+            Flip ();
+	}
+	
+	void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
